fd04b7cecd8ce630568f961031e34cc67161058e
Cristina <cristina.gratton@gmail.com>
2021-12-05 14:00:05 -0600

    ==

--- scriptHelper.js
+++ scriptHelper.js
@@ -19,7 +19,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 function validateInput(testInput) {
    if (isNaN(testInput)) {
        return "Not a Number";
-   } else if (testInput = "") {
+   } else if (testInput === "") {
         return "Empty";
    } else {
         return "Is a Number";
